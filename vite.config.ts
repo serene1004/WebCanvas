27589ceff826b8d0de0b3ada85dfc95e231bce10
serene1004 <vcbxnzn@naver.com>
2026-07-26 17:12:19 +0900

    build(vite): expose dev server on LAN

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({ base: '/WebCanvas/', plugins: [vue()], server: { port: 4080 } });
+export default defineConfig({ base: '/WebCanvas/', plugins: [vue()], server: { host: true, port: 4080 } });
